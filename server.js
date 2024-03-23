2f51593eeed550c682500f9ca9b351e7a6f28d8c
Deep Trivedi <deepbtrivedi04@gmail.com>
2024-03-23 21:06:41 +0530

    last commit

--- server.js
+++ server.js
@@ -1,12 +1,93 @@
-const express= require('express')
-const app= express()
+const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
+const app = express()
 
-//Route 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.get('/',(req, res)=>{
-   res.end("App is running") 
-} )
+//routes
 
-app.listen(3000, ()=>{
-    console.log("Node API is running on port 3000")
+app.get('/', (req, res) => {
+    res.send('Hello NODE API')
+})
+
+app.get('/blog', (req, res) => {
+    res.send('This is some random blog ')
+})
+
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+app.post('/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+// update a product
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        // we cannot find any product in database
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// delete a product
+
+app.delete('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect('mongodb+srv://deepbtrivedi04:01092004Deep@restfulapi.fjpeitp.mongodb.net/?retryWrites=true&w=majority&appName=restfulAPI')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log(`Node API app is running on port 3000`)
+    });
+}).catch((error) => {
+    console.log(error)
 })
